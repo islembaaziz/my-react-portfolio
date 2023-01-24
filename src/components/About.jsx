28bf12c36d1a7bc594d4238ebd4eea48b8baf1b4
Islem Baaziz <islembaaziz@yahoo.fr>
2023-01-24 14:44:06 +0100

    files

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+        Things you maybe want to know about me! 
+
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+        I had my license in computer science applied to management in 2021 and
+            now, I'm nearly graduated in an Ecommerce Master professional, I'm a 
+            self learner who try always to be up-to-date.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
